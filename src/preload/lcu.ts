b65eff611079bf1d29faef2817be28f376bf1b74
小恐龙 <1363931801@qq.com>
2023-05-05 18:36:26 +0800

    改为http请求

--- src/preload/lcu.ts
+++ src/preload/lcu.ts
@@ -1,15 +1,26 @@
 import axios, { AxiosInstance, AxiosResponse } from 'axios'
-
-function createClient(config: { port: number; token: string }): AxiosInstance {
-  return axios.create({
-    baseURL: `https://172.16.3.134:${config.port}`,
+import https from 'https'
+function createClient(config: { port: number; token: string }) {
+  //   return axios.create({
+  //     baseURL: `https://172.16.3.134:${config.port}`,
+  //     headers: {
+  //       Accept: 'application/json',
+  //       'Content-Type': 'application/json',
+  //       Authorization: 'Basic ' + Buffer.from(`riot:${config.token}`).toString('base64')
+  //     }
+  //   })
+  return https.request({
+    host: '127.0.0.1',
+    port: config.port,
+    path: '/lol-matchmaking/v1/ready-check/accept',
+    method: 'POST',
     headers: {
       Accept: 'application/json',
       'Content-Type': 'application/json',
-      Authorization: Buffer.from(`riot:${config.token}`).toString('base64')
+      Authorization: 'Basic ' + Buffer.from(`riot:${config.token}`).toString('base64')
     }
   })
 }
 export function autoReplay<T>(config: { port: number; token: string }): Promise<AxiosResponse<T>> {
-  return createClient(config).post('/lol-matchmaking/v1/ready-check/accept')
+  return createClient(config)
 }
